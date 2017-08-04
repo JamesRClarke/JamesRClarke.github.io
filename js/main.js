c45cc71c0d34f956432e4fdfcf765b916161e8fc
James Clarke <jamesraymondclarke@gmail.com>
2017-08-04 15:55:01 +0100

    making minor changes to sass, bit of refactor

--- js/main.js
+++ js/main.js
@@ -17,20 +17,13 @@ $(() => {
   $('nav a').on('click', scrollToSection);
 
   function scrollToSection () {
-    const section = $(this).attr('href'); //#gallery
+    const section = $(this).attr('href');
     $('body').animate({
-      scrollTop: $(section).offset().top - 40
+      scrollTop: $(section).offset().top - 50
     }, 2000, () => {
       if($(window).width()<575) {
         $('.dropdown').slideToggle();
       }
     });
   }
-
-  $('.menu').on('click', toggleMenu);
-
-  function toggleMenu() {
-    $('.dropdown').slideToggle();
-  }
-
 });
